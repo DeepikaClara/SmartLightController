c579a5d472b23b7edec3f0aa82c2fe7246592af9
Dpka_rv <97511874+DeepikaClara@users.noreply.github.com>
2026-07-06 23:09:41 +0530

    Add files via upload
    Initial Commit

---
+++ light.h
@@ -0,0 +1,12 @@
+#ifndef LIGHT_H
+#define IIGHT_H
+
+typedef enum{
+
+    ON,
+    OFF
+} light_status;
+
+
+void switch_light();
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "light.h"
+
+int main()
+{
+    switch_light();
+    return 0;
+}
